f2318a4d107315fd9f634b51703b4906346aeedc
AllanLopes2018 <allanlopes2001@gmail.com>
2018-09-20 22:38:24 -0300

    JAVA DO CARALHO

---
+++ Java1de1/src/NovoJavaVariaveis.java
@@ -0,0 +1,24 @@
+public class NovoJavaVariaveis
+{
+    public static void main(String[] args) 
+    {
+        //textos
+        
+        char letra = 'a';
+        String texto = "texto";
+        
+        //logico
+        
+        boolean valor = true; // ou false
+        
+        //numerico
+        
+        int inteiro = 10;
+        
+        long inteiroDobrado = 20;
+        
+        float real = 5.0f;
+        
+        double realDobrado = 20.0;
+    }
+}
